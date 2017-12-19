852c83b02c8e5db5e1dccbf0220cc22548c79930
Candison <kandisheng@163.com>
2017-12-19 14:12:41 +0800

    Update

---
+++ script/Clean.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * CodeMommy ConfigPHP
+ * @author Candison November <www.kandisheng.com>
+ */
+
+namespace CodeMommy\ConfigPHP\Script;
+
+use CodeMommy\TaskPHP\Console;
+use CodeMommy\TaskPHP\FileSystem;
+
+/**
+ * Class Clean
+ * @package CodeMommy\ConfigPHP\Script;
+ */
+class Clean
+{
+    /**
+     * Clean constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Start
+     */
+    public static function start()
+    {
+        $removeList = array(
+            '.report'
+        );
+        $result = FileSystem::remove($removeList);
+        if ($result) {
+            Console::printLine('Clean Finished.', 'success');
+        } else {
+            Console::printLine('Clean Error.', 'error');
+        }
+    }
+}
